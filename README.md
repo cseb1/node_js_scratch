6fe29059f438dab6e49027e9d889066c82fe42e4
Susanta Maity <110307105+cseb1@users.noreply.github.com>
2024-07-14 07:00:44 +0000

    how to run nodeJS file

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # node_js_scratch
 this is a beginner to master NodeJS
+
+# First class
+```
+1.How to run nodeJS file
+2.How to create Json file
+```
---
+++ helloWorld.js
@@ -0,0 +1,7 @@
+console.log("Hii ! i am JS");
+
+// nodeJS does not allow any DOM related or Window related purpose
+// It's actually working for server side
+
+// for example alert method does not working
+//console.log(alert('click'));
